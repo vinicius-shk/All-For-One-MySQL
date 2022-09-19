18648b34daf2b47906b6aaef08187388a25d6578
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-09-19 16:03:13 -0300

    Requisito 21

---
+++ desafio21.sql
@@ -0,0 +1,4 @@
+INSERT INTO northwind.order_details(date_allocated, discount, inventory_id, order_id, product_id, purchase_order_id, quantity, status_id, unit_price)
+VALUE
+(NULL, 0, 129, 69, 80, NULL, 15.0000, 2, 15.0000),
+(NULL, 0, 129, 69, 80, NULL, 15.0000, 2, 15.0000);
