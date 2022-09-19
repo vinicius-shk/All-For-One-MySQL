b18142365da5db2c9cce5d12cb23dd3152cacda7
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-09-19 17:55:26 -0300

    Refatorando os reqs 20 e 21 baseado no CR do Lauro Pereira

--- desafio21.sql
+++ desafio21.sql
@@ -1,4 +1,4 @@
-INSERT INTO northwind.order_details(date_allocated, discount, inventory_id, order_id, product_id, purchase_order_id, quantity, status_id, unit_price)
+INSERT INTO northwind.order_details(discount, inventory_id, order_id, product_id, quantity, status_id, unit_price)
 VALUE
-(NULL, 0, 129, 69, 80, NULL, 15.0000, 2, 15.0000),
-(NULL, 0, 129, 69, 80, NULL, 15.0000, 2, 15.0000);
+(0, 129, 69, 80, 15.0000, 2, 15.0000),
+(0, 129, 69, 80, 15.0000, 2, 15.0000);
